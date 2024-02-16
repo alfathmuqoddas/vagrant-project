734800723e961a88ec25598930b076a1ac313b01
Alfath Muqoddas <85182650+alfathmuqoddas@users.noreply.github.com>
2024-02-16 13:46:29 +0700

    Update script.sh

--- debian-buster-dwm/script.sh
+++ debian-buster-dwm/script.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
 echo "simple update and upgrade"
-apt update && apt upgrade -y > /dev/null
+sudo apt update && sudo apt upgrade -y > /dev/null
 
 echo "install dependecies"
-apt install libx11-dev libxft-dev libxinerama-dev build-essential -y > /dev/null
+sudo apt install libx11-dev libxft-dev libxinerama-dev build-essential -y > /dev/null
 
 echo "install xorg and display manager"
-apt install xserver-xorg-video-radeon xserver-xorg-core lightdm lightdm-gtk-greeter pcmanfm firefox-esr git lxappearance xarchiver pulseaudio pavucontrol -y > /dev/null
+sudo apt install xserver-xorg-video-radeon xserver-xorg-core lightdm lightdm-gtk-greeter pcmanfm git lxappearance xarchiver pulseaudio pavucontrol -y > /dev/null
 
 echo "enable lightdm"
-systemctl enable lightdm > /dev/null
+sudo systemctl enable lightdm > /dev/null
 
 echo "cloning some repo"
 echo simple-wallpaper alfath-dwm alfath-dwmstatus alfath-st1 alfath-dmenu fonts-cool yaru-look | xargs -n1 | xargs -I{} git clone https://github.com/alfathmuqoddas/{} > /dev/null
 
 echo "copy repo font"
-cp -r fonts-cool/* /usr/share/fonts > /dev/null
+sudo cp -r fonts-cool/* /usr/share/fonts > /dev/null
 
 echo "copy repo theme"
-cp -r yaru-look/Themes/* /usr/share/themes > /dev/null
+sudo cp -r yaru-look/Themes/* /usr/share/themes > /dev/null
 
 echo "install numix icon theme"
-apt install numix-icon-theme -y > /dev/null
+sudo apt install numix-icon-theme -y > /dev/null
 
 echo "installing dwm"
-mkdir -p .dwm && mkdir -p /home/vagrant/.dwm && cd /home/vagrant/alfath-dwm && make clean install && cp autostart.sh ../.dwm && cp autostart.sh /root/.dwm && cp dwm.desktop /usr/share/xsessions > /dev/null
+mkdir -p .dwm && mkdir -p /home/alfath/.dwm && cd /home/alfath/alfath-dwm && sudo make clean install && cp autostart.sh ../.dwm && sudo cp autostart.sh /root/.dwm && sudo cp dwm.desktop /usr/share/xsessions > /dev/null
 
 echo "installing st1"
-cd /home/vagrant/alfath-st1 && make clean install > /dev/null
+cd /home/alfath/alfath-st1 && sudo make clean install > /dev/null
 
 echo "installing dwmstatus"
-cd /home/vagrant/alfath-dwmstatus && make clean install > /dev/null
+cd /home/alfath/alfath-dwmstatus && sudo make clean install > /dev/null
 
 echo "installing dmenu"
-cd /home/vagrant/alfath-dmenu && make clean install > /dev/null
+cd /home/alfath/alfath-dmenu && sudo make clean install > /dev/null
 
 echo "reboot"
 sudo reboot
